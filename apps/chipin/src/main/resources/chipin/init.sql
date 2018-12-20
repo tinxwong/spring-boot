15bac3301ab096d439567d0e9392e2545967214f
王天喣 <tianxu.wang@meicloud.com>
2018-12-20 15:58:03 +0800

    1、chipin模块的sql语句添加了insert内容

--- apps/chipin/src/main/resources/chipin/init.sql
+++ apps/chipin/src/main/resources/chipin/init.sql
@@ -37,6 +37,16 @@ CREATE TABLE `chipin_config` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `chipin_config`
+--
+
+LOCK TABLES `chipin_config` WRITE;
+/*!40000 ALTER TABLE `chipin_config` DISABLE KEYS */;
+INSERT INTO `chipin_config` VALUES (1,'LOTTERY','KEY','asdWE3ac',NULL,'2018-08-10 22:29:27',NULL,'2018-08-10 22:29:27',1),(3,'LOTTERY','JINJIU','http://k1.cq6055.xyz,http://k2.cq6055.xyz,http://k3.cq6055.xyz,http://k4.cq6055.xyz,http://k5.cq6055.xyz,http://k6.cq6055.xyz,http://k7.cq6055.xyz,http://k8.cq6055.xyz',NULL,'2018-08-30 22:10:28',NULL,'2018-08-30 22:10:28',1),(4,'TEMP','LOTTERY','D:\\temp',NULL,'2018-10-14 20:51:09',NULL,NULL,1);
+/*!40000 ALTER TABLE `chipin_config` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `chipin_log`
 --
 
@@ -72,6 +82,16 @@ CREATE TABLE `chipin_log` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `chipin_log`
+--
+
+LOCK TABLES `chipin_log` WRITE;
+/*!40000 ALTER TABLE `chipin_log` DISABLE KEYS */;
+INSERT INTO `chipin_log` VALUES (26,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216074','2018-12-16 10:11:03',NULL,NULL,NULL,'下注成功','2764.8','',NULL,1,'{start=2018-12-16 18:21:30, end=2018-12-16 18:29:00}','1970-01-19 05:09:15',1),(27,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216075','2018-12-16 10:23:23',NULL,NULL,NULL,'下注成功','2764.7','',NULL,1,'{start=2018-12-16 18:31:30, end=2018-12-16 18:39:00}','1970-01-19 05:09:16',1),(28,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216076','2018-12-16 10:34:11',NULL,NULL,NULL,'下注成功','2764.6','',NULL,1,'{start=2018-12-16 18:41:30, end=2018-12-16 18:49:00}','1970-01-19 05:09:17',1),(29,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216077','2018-12-16 10:48:36',NULL,NULL,NULL,'下注成功','2764.5','defaultChipinRule',NULL,1,'{start=2018-12-16 18:51:30, end=2018-12-16 18:59:00}','2018-12-16 18:57:32',1),(30,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216078','2018-12-16 10:57:33',NULL,NULL,NULL,'下注成功','2764.4','defaultChipinRule',NULL,1,'{start=2018-12-16 19:01:30, end=2018-12-16 19:09:00}','2018-12-16 19:04:19',1),(31,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216079','2018-12-16 11:04:20',NULL,NULL,NULL,'下注成功','2764.3','defaultChipinRule',NULL,1,'{start=2018-12-16 19:11:30, end=2018-12-16 19:19:00}','2018-12-16 19:13:37',1),(32,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216080','2018-12-16 11:13:38',NULL,NULL,NULL,'下注成功','2764.2','defaultChipinRule',NULL,1,'{start=2018-12-16 19:21:30, end=2018-12-16 19:29:00}','2018-12-16 19:25:19',1),(33,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216081','2018-12-16 11:25:20',NULL,NULL,NULL,'下注成功','2764.1','defaultChipinRule',NULL,1,'{start=2018-12-16 19:31:30, end=2018-12-16 19:39:00}','2018-12-16 19:35:51',1),(34,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216099','2018-12-16 14:10:56',NULL,NULL,NULL,'下注成功','2764','defaultChipinRule',NULL,1,'{start=2018-12-16 22:16:30, end=2018-12-16 22:19:00}','2018-12-16 22:18:29',1),(35,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216100','2018-12-16 14:18:29',NULL,NULL,NULL,'下注成功','2763.9','defaultChipinRule',NULL,1,'{start=2018-12-16 22:21:30, end=2018-12-16 22:24:00}','2018-12-16 22:23:13',1),(36,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216101','2018-12-16 14:23:14',NULL,NULL,NULL,'下注成功','2763.8','defaultChipinRule',NULL,1,'{start=2018-12-16 22:26:30, end=2018-12-16 22:29:00}','2018-12-16 22:28:58',1),(37,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216102','2018-12-16 14:28:58',NULL,NULL,NULL,'下注成功','2763.7','defaultChipinRule',NULL,1,'{start=2018-12-16 22:31:30, end=2018-12-16 22:34:00}','2018-12-16 22:32:36',1),(38,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216103','2018-12-16 14:32:37',NULL,NULL,NULL,'下注成功','2761.2','defaultChipinRule',NULL,1,'{start=2018-12-16 22:36:30, end=2018-12-16 22:39:00}','2018-12-16 22:38:54',1),(39,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216104','2018-12-16 14:38:56',NULL,NULL,NULL,'下注成功','2758.7','defaultChipinRule',NULL,1,'{start=2018-12-16 22:41:30, end=2018-12-16 22:44:00}','2018-12-16 22:42:47',1),(40,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216105','2018-12-16 14:42:48',NULL,NULL,NULL,'下注成功','2758.6','defaultChipinRule',NULL,1,'{start=2018-12-16 22:46:30, end=2018-12-16 22:49:00}','2018-12-16 22:48:25',1),(41,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216106','2018-12-16 14:48:26',NULL,NULL,NULL,'下注成功','2753.7','defaultChipinRule',NULL,1,'{start=2018-12-16 22:51:30, end=2018-12-16 22:54:00}','2018-12-16 22:52:15',1),(42,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216107','2018-12-16 14:52:15',NULL,NULL,NULL,'下注成功','2753.6','defaultChipinRule',NULL,1,'{start=2018-12-16 22:56:30, end=2018-12-16 22:59:00}','2018-12-16 22:56:51',1),(43,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216108','2018-12-16 14:56:52',NULL,NULL,NULL,'下注成功','2751.1','defaultChipinRule',NULL,1,'{start=2018-12-16 23:01:30, end=2018-12-16 23:04:00}','2018-12-16 23:03:46',1),(44,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216109','2018-12-16 15:03:46',NULL,NULL,NULL,'下注成功','2746.2','defaultChipinRule',NULL,1,'{start=2018-12-16 23:6:30, end=2018-12-16 23:9:00}','2018-12-16 23:06:54',1),(45,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216110','2018-12-16 15:06:56',NULL,NULL,NULL,'下注成功','2743.7','defaultChipinRule',NULL,1,'{start=2018-12-16 23:11:30, end=2018-12-16 23:14:00}','2018-12-16 23:12:08',1),(46,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216111','2018-12-16 15:12:08',NULL,NULL,NULL,'下注成功','2743.6','defaultChipinRule',NULL,1,'{start=2018-12-16 23:16:30, end=2018-12-16 23:19:00}','2018-12-16 23:17:49',1),(47,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181216112','2018-12-16 15:17:50',NULL,NULL,NULL,'下注成功','2741.1','defaultChipinRule',NULL,1,'{start=2018-12-16 23:21:30, end=2018-12-16 23:24:00}','2018-12-16 23:22:31',1),(48,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181218097','2018-12-18 14:00:45',NULL,NULL,NULL,'A JSONObject text must begin with \'{\' at character 1 of <script>document.domain = document.domain;</script>{\"Status\":1,\"Data\":{\"upload_type\":0,\"Details\":[{\"bet_no\":\"0005\",\"bet_money\":\"\",\"dict_no_type_id\":11}],\"TotalCount\":1,\"TotalBetMoney\":\"0\"}}','2749.2','defaultChipinRule',NULL,0,'{start=2018-12-18 22:6:30, end=2018-12-18 22:9:00}','2018-12-18 22:07:49',1),(49,2,'kai',NULL,NULL,8,NULL,'0.10',NULL,NULL,'20181218097','2018-12-18 14:04:30',NULL,NULL,NULL,'A JSONObject text must begin with \'{\' at character 1 of <script>document.domain = document.domain;</script>{\"Status\":2,\"Data\":\"txt导入暂未开放\"}','2749.2','defaultChipinRule',NULL,0,'{start=2018-12-18 22:6:30, end=2018-12-18 22:9:00}','2018-12-18 22:08:48',1);
+/*!40000 ALTER TABLE `chipin_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `lottery`
 --
 
@@ -103,6 +123,16 @@ CREATE TABLE `lottery` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `lottery`
+--
+
+LOCK TABLES `lottery` WRITE;
+/*!40000 ALTER TABLE `lottery` DISABLE KEYS */;
+INSERT INTO `lottery` VALUES (1,'/Member/DoLogin?_=','/Member/DoLogin?_=','/drawno/GetCurrentPeriodStatus?_=','/Member/GetMemberPrint?_=','金玖网站','金玖投注网站','2018-08-31 22:51:53','admin',NULL,'2018-12-06 08:39:11',1,'/Member/AcceptAgreement?_=','/drawno/GetCurrentPeriodStatus?_=','/DrawNo/GetDrawNoTable?pageindex=1&_=','http://k1.cq6055.xyz,http://k2.cq6055.xyz,http://k3.cq6055.xyz,http://k4.cq6055.xyz,http://k5.cq6055.xyz,http://k6.cq6055.xyz,http://k7.cq6055.xyz,http://k8.cq6055.xyz','jinJiuAuthent','/Member/BatchBet','/Member/UpLoadBetNos'),(2,'/Member/DoLogin?_=','/Member/DoLogin?_=','/drawno/GetCurrentPeriodStatus?_=','/Member/GetMemberPrint?_=','六六六网站','六六六投注网站','2018-10-08 22:34:47','admin',NULL,'2018-12-19 01:34:19',1,'/Member/AcceptAgreement?_=','/drawno/GetCurrentPeriodStatus?_=','/DrawNo/GetDrawNoTable?pageindex=1&_=','http://ff1.qq18718722.com,http://ff2.qq18718722.com,http://ff3.qq18718722.com,http://ff4.qq18718722.com,http://ff5.qq18718722.com,http://ff6.qq18718722.com,http://ff7.qq18718722.com,http://ff8.qq18718722.com','liuLiuLiuAuthent','/Member/BatchBet','/Member/UpLoadBetNos'),(3,'/Member/DoLogin?_=','/Member/DoLogin?_=','/drawno/GetCurrentPeriodStatus?_=','/Member/GetMemberPrint?_=','新永利','新永利投注网站','2018-10-08 22:34:47','admin',NULL,'2018-12-16 09:10:32',1,'/Member/AcceptAgreement?_=','/drawno/GetCurrentPeriodStatus?_=','/DrawNo/GetDrawNoTable?pageindex=1&_=','http://1y.cq1555.xyz,http://2y.cq1555.xyz,http://3y.cq1555.xyz,http://4y.cq1555.xyz,http://5y.cq1555.xyz,http://6y.cq1555.xyz,http://7y.cq1555.xyz,http://8y.cq1555.xyz','liuLiuLiuAuthent','/Member/BatchBet','/Member/UpLoadBetNos');
+/*!40000 ALTER TABLE `lottery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `migration`
 --
 
@@ -117,6 +147,16 @@ CREATE TABLE `migration` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migration`
+--
+
+LOCK TABLES `migration` WRITE;
+/*!40000 ALTER TABLE `migration` DISABLE KEYS */;
+INSERT INTO `migration` VALUES (1,'chipin/init.sql','8858f8411ab709e0d547875a8f071391','2018-12-04 09:39:04'),(2,'chipin/20181212.sql','fdbb905576d8d4e5b724ec6a5c53d122','2018-12-12 10:28:24');
+/*!40000 ALTER TABLE `migration` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `task`
@@ -153,6 +193,16 @@ CREATE TABLE `task` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+INSERT INTO `task` VALUES (5,1,1,800,400,10,1.00,'2018-10-19 15:19:30',NULL,NULL,'2018-10-16 23:38:40',1,0,'D:\\temp/1/5/1/151.txt','3','','往期号码规则','periodNumberChipinRule','齐天大圣','金玖网站',0,0),(6,2,2,3600,1000,10,0.10,'2018-12-19 01:50:03',NULL,'kai','2018-12-19 01:50:03',1,0,'D:\\temp/2/6/4/264.txt','1','','默认规则','defaultChipinRule','kai','六六六网站',0,0),(7,3,1,400,100,NULL,1.00,'2018-12-05 15:12:13',NULL,NULL,'2018-12-05 15:12:13',0,0,'D:\\temp/1/7/3/173.txt','1','','默认规则','defaultChipinRule','zhangshan','金玖网站',0,0),(8,2,3,4500,0,NULL,0.10,'2018-12-19 01:28:26',NULL,NULL,'2018-12-19 01:28:26',1,0,'D:\\temp/3/8/2/382.txt','1','','默认规则','defaultChipinRule','kai','新永利',0,0);
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -179,6 +229,16 @@ CREATE TABLE `user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'齐天大圣','王某','e10adc3949ba59abbe56e057f20f883e','13503098948','1','COMMON','2018-10-13 23:29:26',NULL,NULL,'2018-08-31 23:16:25',1,2),(2,'天蓬元帅','猪八戒','e10adc3949ba59abbe56e057f20f883e','','1','COMMON','2018-10-13 23:29:26',NULL,NULL,'2018-10-13 22:55:50',1,2),(3,'admin','管理员','e10adc3949ba59abbe56e057f20f883e','','1','ADMIN','2018-10-13 23:42:03',NULL,NULL,'2018-10-13 23:21:16',1,1),(4,'kai','恺','e10adc3949ba59abbe56e057f20f883e','','1','COMMON','2018-10-20 16:25:37',NULL,NULL,'2018-10-20 16:25:02',1,2);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user_lottery`
 --
 
@@ -203,6 +263,16 @@ CREATE TABLE `user_lottery` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `user_lottery`
+--
+
+LOCK TABLES `user_lottery` WRITE;
+/*!40000 ALTER TABLE `user_lottery` DISABLE KEYS */;
+INSERT INTO `user_lottery` VALUES (4,2,2,'yy0055','aaaa11111','2018-12-19 01:31:48','admin','admin',NULL,'kai','六六六网站',1),(5,2,3,'hh3555','aaaa11111','2018-12-16 09:11:02','admin',NULL,NULL,'kai','新永利',1);
+/*!40000 ALTER TABLE `user_lottery` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user_lottery_rule`
 --
 
@@ -225,6 +295,15 @@ CREATE TABLE `user_lottery_rule` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_lottery_rule`
+--
+
+LOCK TABLES `user_lottery_rule` WRITE;
+/*!40000 ALTER TABLE `user_lottery_rule` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_lottery_rule` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -235,4 +314,4 @@ CREATE TABLE `user_lottery_rule` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-20 15:45:32
+-- Dump completed on 2018-12-20 15:56:13
